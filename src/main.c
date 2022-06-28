229b73df1f4a667781ce62b1a5aee315a3c62140
Suirabu <suirabu.dev@gmail.com>
2022-06-27 23:24:54 -0700

    Allow the parsing of Cave Story+ '.dat' files

--- src/main.c
+++ src/main.c
@@ -18,15 +18,24 @@ int main(int argc, char** argv) {
         options.file_path = "290.rec";
     }
 
-    // Decode rec file contents
-    int result = rec_decode(options.file_path);
+    // Decode file contents
+    int result;
+    if(options.csplus) {
+        result = dat_decode(options.file_path);
+    } else {
+        result = rec_decode(options.file_path);
+    }
+
     if(result == -1) {
         return EXIT_FAILURE;
     }
 
     // Display results
-    const int minutes = result / FRAMES_PER_MINUTE;
-    const double seconds = (double)(result % FRAMES_PER_MINUTE) / FRAMES_PER_SECOND;
+    const int frames_per_second = options.csplus ? 60 : 50;
+    const int frames_per_minute = frames_per_second * 60;
+
+    const int minutes = result / frames_per_minute;
+    const double seconds = (double)(result % frames_per_minute) / frames_per_second;
 
     printf("\033[0;1mTime:\033[0m   %02u:%.2lf\n", minutes, seconds);
     printf("\033[0;1mFrames:\033[0m %u\n", result);
